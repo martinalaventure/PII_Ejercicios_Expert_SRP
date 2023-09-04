1995a9529ffeb77ccf22552414e04b4afee1dfbe
Martina Laventure <martina.laventure@correo.ucu.edu.uy>
2023-09-04 11:18:53 -0300

    Prueba1

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,17 +7,13 @@ namespace Program
     {
         static void Main()
         {
-            Appointment appointment = new Appointment
-            {
-                name = "Martina Laventure",
-                id = "5571876-9",
-                phoneNumber = "098994321",
-                date= DateTime.Now,
-                appointmentPlace = "Clinic A",
-                doctorName = "Jorge Ruiz"
-            };
+            Appointment appointment = new Appointment(DateTime.Now, "Clinic A");
+            Patient patient = new Patient("Martina Laventure","5571876-9","098994321");
+            Doctor doctor= new Doctor("Fernando Ruiz","Dermatologist");
+            Validation validation = new Validation();
+
             AppointmentScheduler scheduler = new AppointmentScheduler();
-            string result = scheduler.ScheduleAppointment(appointment);
+            string result = scheduler.ScheduleAppointment(patient, doctor, appointment, validation);
 
             Console.WriteLine(result);
         }
